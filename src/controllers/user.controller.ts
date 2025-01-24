2e40f2afebbf0b4ef8bb2d1668af81cbe1c69a19
Varun Arora <aroravaruncode@gmail.com>
2025-01-24 22:20:19 +0530

    add logout feature

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,9 +4,12 @@ import { User } from "../models/user.model";
 import { ApiError } from "../utils/ApiError";
 import { ApiResponse } from "../utils/ApiResponse";
 import jwt from "jsonwebtoken";
+import { options } from "../contants";
 
 const signUpUser = asyncHandler(async (req: Request, res: Response) => {
   const { username, password } = req.body;
+  console.log("1");
+  console.log(username);
   if (await User.findOne({ username })) {
     throw new ApiError(400, "User already exists");
   }
@@ -34,7 +37,15 @@ const signInUser = asyncHandler(async (req: Request, res: Response) => {
   );
   res
     .status(200)
-    .json(new ApiResponse(200, { token }, "User logged in successfully"));
+    .cookie("token", token, options)
+    .json(new ApiResponse(200, { user }, "User logged in successfully"));
 });
 
-export { signUpUser, signInUser };
+const logOutUser = asyncHandler(async (req: Request, res: Response) => {
+  res
+    .status(200)
+    .clearCookie("token")
+    .json(new ApiResponse(200, {}, "User logged out successfully"));
+});
+
+export { signUpUser, signInUser, logOutUser };
